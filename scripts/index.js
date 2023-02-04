ec362dda9b429f9beb320819a33cc7a095eed414
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-02-04 11:28:53 -0700

    javascript updates

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const initialCards = [object1, object2, object3, object4, object5, object6];
+let object1 = {
+  name: YosemiteValley,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+  ),
+};
+let object2 = {
+  name: LakeLouise,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+  ),
+};
+let object3 = {
+  name: BaldMountains,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+  ),
+};
+let object4 = {
+  name: Latemar,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+  ),
+};
+let object5 = {
+  name: VanoiseNationalPark,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+  ),
+};
+let object6 = {
+  name: LagodiBraies,
+  link: URL(
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg "
+  ),
+};
