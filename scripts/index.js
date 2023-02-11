0011b0ce1d4377d1cbab66085dcbf00ee187e561
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-02-11 10:49:41 -0700

    js updates

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
