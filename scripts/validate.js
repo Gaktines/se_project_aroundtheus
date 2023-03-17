0788796f21ebd84e4d5bb08db4d6f81de8183b46
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-03-17 12:23:09 -0600

    js updates

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,52 +1,66 @@
+const saveButton = document.querySelector(".modal__button");
+const validationOptions = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
 function showInputError(modalElment, inputElement, validationOptions) {
-const errorMessageElement = document.querySelector(`#${inputElement.id}-error`);
-inputElement.classList.add(validationOptions.inputErrorClass);
-errorMessageElement.textContent = inputElement.validationMessage;
-errorMessageElement.classList.add(validationOptions.errorClass);
+  const errorMessageElement = document.querySelector(
+    `#${inputElement.id}-error`
+  );
+  inputElement.classList.add(validationOptions.inputErrorClass);
+  errorMessageElement.textContent = inputElement.validationMessage;
+  errorMessageElement.classList.add(validationOptions.errorClass);
 }
 
 function hideInputError(modalElment, inputElement, validationOptions) {
-  const errorMessageElement = document.querySelector(`#${inputElement.id}-error`);
+  const errorMessageElement = document.querySelector(
+    `#${inputElement.id}-error`
+  );
   inputElement.classList.remove(validationOptions.inputErrorClass);
   errorMessageElement.textContent = "";
   errorMessageElement.classList.remove(validationOptions.errorClass);
-  }
+}
 
 function checkInputValidity(modalElment, inputElement, validationOptions) {
-if (!inputElement.validity.valid) {
-  showInputError(modalElment, inputElement, validationOptions);
-} else {
-  hideInputError(modalElment, inputElement, validationOptions);
+  if (!inputElement.validity.valid) {
+    showInputError(modalElment, inputElement, validationOptions);
+  } else {
+    hideInputError(modalElment, inputElement, validationOptions);
+  }
 }
+function isNotValid(inputList) {
+  return inputList.every((inputElement) => !inputElement.validity.valid);
 }
 
+function disableSaveButton() {}
+
+function enableSaveButton() {}
 function toggleButtonState(inputElements, saveButton, validationOptions) {
-let notValid = false;
-inputElements.forEach(inputElement => {
-if (!inputElement.validity.valid) {
-  notValid = true;
+  if (isNotValid(inputElements)) {
+    saveButton.classList.add(validationOptions.inactiveButtonClass);
+    saveButton.disabled = true;
+  } else {
+    saveButton.classList.remove(validationOptions.inactiveButtonClass);
+    saveButton.disabled = false;
+  }
 }
-});
-if (notValid) {
-  saveButton.classList.add(validationOptions.inactiveButtonClass);
-  saveButton.disabled= true;
-}else {
-  saveButton.classList.remove(validationOption.inactiveButtonClass);
-  saveButton.disabled= false;
-}
-}
-
 function setEventListeners(modalElement, validationOptions) {
   const inputElements = Array.from(
-    modalElement.querySelectorAll(validationOptions.inputSelector));
-    const saveButton = modalElement.querySelector(".modal__button");
-    inputElements.forEach((inputElement) => {
+    modalElement.querySelectorAll(validationOptions.inputSelector)
+  );
+
+  inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", (evt) => {
-checkInputValidity(modalElment, inputElement, validationOptions);
-toggleButtonState(inputElements, saveButton, validationOptions);
+      checkInputValidity(modalElement, inputElement, validationOptions);
+      toggleButtonState(inputElements, saveButton, validationOptions);
     });
   });
-
+}
 function enableValidation(validationOptions) {
   const modalElements = Array.from(
     document.querySelectorAll(validationOptions.formSelector)
@@ -59,14 +73,4 @@ function enableValidation(validationOptions) {
   });
 }
 
-
-const validationOptions = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
 enableValidation(validationOptions);
-
