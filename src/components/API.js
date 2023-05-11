aa638e2c5dc25ab248cb6b710442227de49c2b9d
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-05-11 10:51:27 -0600

    js updates

---
+++ src/components/API.js
@@ -0,0 +1,50 @@
+fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
+  headers: {
+    authorization: "bb2f5d86-90ca-441b-9ac8-a1ee02058df5"
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+  });
+
+  class Api {
+    constructor(name, link) {
+      this.name = name;
+      this.link = link;
+    }
+  
+    getInitialCards() {
+      fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
+  headers: {
+    authorization: "bb2f5d86-90ca-441b-9ac8-a1ee02058df5"
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+  });
+
+  
+    
+    }
+  
+    // other methods for working with the API
+  }
+  
+  const api = new Api({
+    baseUrl: "https://around.nomoreparties.co/v1/group-12",
+    headers: {
+      authorization: "bb2f5d86-90ca-441b-9ac8-a1ee02058df5",
+      "Content-Type": "application/json"
+    }
+  });
+
+  api.getInitialCards()
+  .then((result) => {
+    // process the result
+    return result.json;
+  })
+  .catch((err) => {
+    console.error(err); // log the error to the console
+  });
