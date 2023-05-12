55822c4f5a30fe458127f1e2518ab74cfb492836
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-05-12 09:56:41 -0600

    js updates

--- src/components/API.js
+++ src/components/API.js
@@ -29,14 +29,32 @@ fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
     
     }
   postInitialCards() {
-
+    fetch("https://around.nomoreparties.co/v1/group-12/cards", {
+      headers: {
+        authorization: "bb2f5d86-90ca-441b-9ac8-a1ee02058df5"
+      }
+    })
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+      });
   }
 
   getProfileInfo() {
 
   }
   postProfileInfo() {
-    
+    fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
+  method: "PATCH",
+  headers: {
+    authorization: "cbb2f5d86-90ca-441b-9ac8-a1ee02058df5",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    name: "George Aktines",
+    about: "Aspiring Software Engineer"
+  })
+});
   }
     // other methods for working with the API
   }
